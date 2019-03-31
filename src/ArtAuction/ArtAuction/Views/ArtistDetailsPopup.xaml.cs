914cdba85d7ce3f221ff02d48a69e5f6ea18d38a
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-03-31 13:35:15 +1100

    Implemented Staggered grid for artwork collection.
    Created structure for Artist details popup but didn't fully implement because the focus of this commit is for the grid.

---
+++ src/ArtAuction/ArtAuction/Views/ArtistDetailsPopup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ArtAuction.Views
+{
+    public partial class ArtistDetailsPopup : ContentView
+    {
+        public ArtistDetailsPopup()
+        {
+            InitializeComponent();
+        }
+        public delegate void ClickExpandDelegate();
+
+        public ClickExpandDelegate OnExpandTapped { get; set; }
+
+        public double FirstSectionHeight
+        {
+            get
+            {
+                return FirstSection.Height;
+            }
+        }
+
+        private void FirstSection_Tapped(object sender, EventArgs e)
+        {
+            OnExpandTapped?.Invoke();
+        }
+    }
+}
